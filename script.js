12d0ea00f3a752d1381f0c7efd990e9b382e4bf0
2947 <2947484@gmail.com>
2026-05-15 15:52:12 +0300

    uploading coutDown project

---
+++ script.js
@@ -0,0 +1,30 @@
+const targetDate = new Date('May 25, 2026 00:00:00').getTime();
+
+const countdownInterval = setInterval(() => {
+    
+    const now = new Date().getTime();
+    
+    const difference = targetDate - now;
+    
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    
+    document.getElementById('days').innerText = days < 10 ? '0' + days : days;
+    document.getElementById('hours').innerText = hours < 10 ? '0' + hours : hours;
+    document.getElementById('minutes').innerText = minutes < 10 ? '0' + minutes : minutes;
+    document.getElementById('seconds').innerText = seconds < 10 ? '0' + seconds : seconds;
+    
+    if (difference < 0) {
+        clearInterval(countdownInterval); 
+        
+        document.getElementById('days').innerText = '00';
+        document.getElementById('hours').innerText = '00';
+        document.getElementById('minutes').innerText = '00';
+        document.getElementById('seconds').innerText = '00';
+        
+        document.getElementById('celebrate-message').style.display = 'block';
+    }
+
+}, 1000);
